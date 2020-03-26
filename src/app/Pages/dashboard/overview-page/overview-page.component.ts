19692aaeca7f4fd2c85a27ffecfd3b588ff1a9f0
Harsimran Singh <52668529+HarsimranDhoofar@users.noreply.github.com>
2020-03-25 21:44:07 -0400

    Some More Changes

--- src/app/Pages/dashboard/overview-page/overview-page.component.ts
+++ src/app/Pages/dashboard/overview-page/overview-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../database/auth/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.css']
 })
 export class OverviewPageComponent implements OnInit {
-
-  constructor() { }
+  user: firebase.User
+  
+  prov: any = []
+  constructor(private router: Router,
+    private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.getUserState()
+     .subscribe( user =>{
+       console.log(user.email)
+      this.user = user;
+    });
+    this.auth.getEmployees().subscribe(prov =>{
+      console.log(prov)
+      this.prov = prov ;
+      console.log(this.prov)
+    })
   }
 
 }
